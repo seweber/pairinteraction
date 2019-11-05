89036b862ea0f243a285972e09561ce19c20aa68
Sebastian Weber <seweber@posteo.de>
2019-11-05 18:21:42 +0100

    [travis] Install bug-free version of twine

--- ci/deploy_pypi.sh
+++ ci/deploy_pypi.sh
@@ -5,10 +5,10 @@ set -e;
 if [ -d $TRAVIS_BUILD_DIR/build/wheelhouse ]; then
   case "$1" in
     "real")
-      twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
+      python3 -m twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
       ;;
     "test")
-      twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing --repository-url https://test.pypi.org/legacy/ $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
+      python3 -m twine upload --username pairinteraction-slave --password $PYPI_TOKEN --skip-existing --repository-url https://test.pypi.org/legacy/ $TRAVIS_BUILD_DIR/build/wheelhouse/*.whl;
       ;;
   esac;
 fi
