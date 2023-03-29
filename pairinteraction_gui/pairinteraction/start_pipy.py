21f1c6eb850f20593cc17be2173d10c706d23a54
moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2023-03-29 14:23:06 +0200

    added start_pipy.py

---
+++ pairinteraction_gui/pairinteraction/start_pipy.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+"""The main function will be called by the pairinteraction GUI to start the calculation.
+"""
+import argparse
+import concurrent.futures
+import itertools
+import json
+import os
+import pickle
+import sys
+from functools import partial
+
+import numpy as np
+from scipy import sparse
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../..")
+import pipy  # noqa
+
+
+def main(args):
+    # Load params from conf.json file
+    with open(args.path_config) as f:
+        conf = json.load(f)
+
+    # convert conf to settings and save as settings.json
+    settings = conf_to_settings(conf, args.path_cache)
+    with open(args.path_config.replace("conf", "settings"), "w") as f:
+        json.dump(settings, f, indent=4)
+
+    # start the calculation
+    config = settings["config"]
+    scriptoptions = settings["scriptoptions"]
+    output(f"{'>>TYP':5}{settings['button_id']:7}")
+    output(f"{'>>TOT':5}{scriptoptions['numBlocks']*scriptoptions['listoptions']['steps']:7}")
+    if config["nAtoms"] == 2:
+        for bn, syms in enumerate(scriptoptions["symmetries_list"]):
+            config.update(syms)
+            settings["blocknumber"] = bn
+            do_simulations(settings)
+    else:
+        settings["blocknumber"] = 1
+        do_simulations(settings)
+    output(f"{'>>END':5}")
+
+
+def do_simulations(settings):
+    param_list = pipy.calc.get_param_list(settings)
+    ip_list = list(range(len(param_list)))
+
+    # TODO init and save atom?
+    atom = pipy.calc.atom_from_config(settings["config"])
+    atom.system
+
+    if atom.nAtoms == 2:
+        allQunumbers = [[*qns[:, 0], *qns[:, 1]] for qns in np.array(atom.allQunumbers)]
+    else:
+        allQunumbers = atom.allQunumbers
+    _name = f"{'one' if atom.nAtoms == 1 else 'two'}_{atom.config.toHash()}"
+    path_basis = f"/tmp/basis_{_name}_blocknumber_{settings['blocknumber']}.csv"
+    basis = np.insert(allQunumbers, 0, np.arange(len(allQunumbers)), axis=1)
+    np.savetxt(path_basis, basis, delimiter="\t", fmt=["%d"] + ["%d", "%d", "%.1f", "%.1f"] * atom.nAtoms)
+    output(f"{'>>BAS':5}{len(basis):7}")
+    output(f"{'>>STA':5} {path_basis} ")
+
+    config = {"atom": atom}
+    p_one_run = partial(one_run, config, param_list)
+
+    num_pr = settings.get("runtimeoptions", {}).get("NUM_PROCESSES", 1)
+    num_pr = os.cpu_count() if num_pr in [0, -1] else num_pr
+    if num_pr > 1:
+        with concurrent.futures.ProcessPoolExecutor(num_pr) as executor:
+            futures = [executor.submit(p_one_run, i) for i in ip_list]
+            for future in concurrent.futures.as_completed(futures):
+                print_completed(settings, future.result())
+    else:
+        for i in ip_list:
+            result = p_one_run(i)
+            print_completed(settings, result)
+
+
+def one_run(config, param_list, ip):
+    # get default Atom from config (either given, load it or newly construct it)
+    if "atom" in config:
+        atom = config["atom"]
+    elif "atom_path" in config:
+        atom = pipy.calc.load_atom(config["atom_path"])
+    else:
+        atom = pipy.calc.atom_from_config(config)
+    config = atom.config
+
+    atom.updateFromParams(param_list[ip])
+    dimension = len(atom.basisQunumbers)
+
+    real_complex = "real" if config.isReal else "complex"
+    pathCacheMatrix = config.pathCache + f"cache_matrix_{real_complex}_new/"
+    os.makedirs(pathCacheMatrix, exist_ok=True)
+    _name = f"{'one' if atom.nAtoms == 1 else 'two'}_{config.toHash()}"
+    filename = pathCacheMatrix + _name + ".pkl"
+
+    if not os.path.exists(filename):
+        output(f"Calculating {ip}, {dimension}x{dimension}")
+        atom.calcEnergies()
+        basis = sparse.csc_matrix(np.sqrt(atom.overlaps))
+        # basis is actually overlaps, naming just for consistency with pairinteraction code
+        energies0 = config.pair.getEnergies() if atom.nAtoms == 2 else config.atom.getEnergies()
+        data = {
+            "energies": atom.energies - np.mean(energies0),
+            "basis": basis,
+            "params": config.toDict(),
+        }
+        with open(filename, "wb") as f:
+            pickle.dump(data, f)
+
+    result = {
+        "ip": ip,
+        "dimension": dimension,
+        "filename": filename,
+    }
+    return result
+
+
+def print_completed(settings, result):
+    output(f"{'>>DIM':5}{result['dimension']:7}")
+    numBlocks = settings["scriptoptions"]["numBlocks"]
+    totalstep = 1 + result["ip"] * numBlocks + settings["blocknumber"]
+    output(
+        f"{'>>OUT':5}{totalstep:7}{result['ip']:7}{numBlocks:7}" f"{settings['blocknumber']:7} {result['filename']} "
+    )
+
+
+def conf_to_settings(conf, pathCache):
+    config = {}
+    for k, v in conf.items():
+        if (
+            k.startswith("delta")
+            or any(k == q + i for q in ["species", "n", "l", "j", "m"] for i in ["", "1", "2"])
+            or k in ["diamagnetism", "samebasis", "exponent"]
+        ):
+            config[k] = v
+
+    config["method"] = ["WHITTAKER", "NUMEROV"][conf["missingCalc"]]
+    config["pathCache"] = pathCache
+    if conf["button_id"] == 2:
+        config["pair.conserveMomenta"] = conf.get("conserveM", False)
+        config["pair.useQunumbersSinglePair"] = True
+        config["nAtoms"] = 2
+    elif conf["button_id"] in [0, 1]:
+        config["nAtoms"] = 1
+        for k in ["species", "n", "l", "j", "m"]:
+            config[k] = config.pop(k + "1", config.pop(k + "2", None))
+    else:  # 3
+        config["nAtoms"] = 1
+    config.update({k: conf.get("min" + k, 0) for k in ["Ex", "Ey", "Ez", "Bx", "By", "Bz"]})
+
+    listoptions = {
+        "onlySameSector": conf.get("sametrafo", None),
+        "steps": conf.get("steps", None),
+        **{mm + "distance": conf.get(mm + "R", None) for mm in ["min", "max"]},
+        **{mm + key: conf.get(mm + key, None) for key in ["Ex", "Ey", "Ez", "Bx", "By", "Bz"] for mm in ["min", "max"]},
+    }
+    config["isReal"] = all([listoptions.get(k, 0) == 0 for k in ["minBy", "maxBy", "minEy", "maxEy"]])
+
+    if config["nAtoms"] == 2:
+        symmetries = {}
+        for s in ["inv", "per", "ref"]:
+            v = [eo for eo in ["O", "E"] if conf.get(s + eo, False)]
+            symmetries[s] = v if len(v) > 0 else [None]
+        symmetries_list = []
+        for inv, per, ref in itertools.product(*[symmetries[k] for k in ["inv", "per", "ref"]]):
+            if listoptions["onlySameSector"]:
+                # In case of inversion and permutation symmetry: the orbital parity is conserved and we
+                # just use the blocks with the same parity as the inital state
+                L = config["l1"] + config["l2"]
+                if (inv + per in ["EO", "OE"] and L % 2 == 0) or (inv + per in ["EE", "OO"] and L % 2 == 1):
+                    continue
+                # TODO add more symmetry conditions here, see HamiltonianTwo.cpp
+            symmetries_list.append({"inversion": inv, "permutation": per, "reflection": ref})
+    else:
+        symmetries_list = [{k: False for k in ["inversion", "permutation", "reflection"]}]
+
+    runtimeoptions = {"NUM_PROCESSES": int(os.environ.get("NUM_PROCESSES", 0))}
+
+    # touch unimportant options
+    for k in ["dd", "dq", "qq", "missingWhittaker", "precision", "zerotheta"]:
+        conf.get(k, None)
+
+    settings = {
+        "config": config,
+        "scriptoptions": {
+            "listoptions": listoptions,
+            "symmetries_list": symmetries_list,
+            "numBlocks": len(symmetries_list),
+        },
+        "runtimeoptions": runtimeoptions,
+        "button_id": conf["button_id"],
+    }
+    return settings
+
+
+def output(x):
+    print(x, flush=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Scripts")
+    parser.add_argument("--run_gui", action="store_true", help="Use this only to run a calculation from the GUI!")
+    parser.add_argument("-c", "--path_config", type=str, help="Path to the config.")
+    parser.add_argument("-o", "--path_cache", type=str, help="Path to the cache.")
+    args = parser.parse_args()
+
+    if args.run_gui:
+        main(args)
+    else:
+        raise ValueError(
+            "For now this script can only be called from the GUI and there is no other use calling this script!"
+        )
