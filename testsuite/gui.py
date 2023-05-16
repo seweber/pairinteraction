ea299098c648aad93f693320fb246873aa3ac8d7
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2023-05-16 17:49:45 +0200

    workaround sanitizer test

--- testsuite/gui.py
+++ testsuite/gui.py
@@ -40,11 +40,9 @@ class PairinteractionGuiTest(unittest.TestCase):
         self.form.loadSettingsPlotter(os.path.join(PATH, "potential", "settings.pconf"))
         self._testEnergies(2, "potential", dE=0.3)
 
-    def _testEnergies(self, idx, ref_data, dE, dE_tol=1e-3, use_python_api="both"):
-        if use_python_api == "both":
-            for use_python_api in [False, True]:
-                self._testEnergies(idx, ref_data, dE, use_python_api=use_python_api)
-            return
+    def _testEnergies(self, idx, ref_data, dE, dE_tol=1e-3, use_python_api="default"):
+        if use_python_api == "default":
+            return self._testEnergies(idx, ref_data, dE, use_python_api=True)
         self.form.ui.checkbox_use_python_api.setChecked(use_python_api)
         self.form.autosetSymmetrization()
 
